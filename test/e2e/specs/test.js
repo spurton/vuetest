e408696a946ac932824be1c7594091cd35a87ff0
Scott Burton <spurton@gmail.com>
2018-03-09 16:57:33 -0800

    Added e2e tests for h3 content and grouping tag

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -10,11 +10,16 @@ module.exports = {
     const today = new Date()
     const todaysDate = `${today.getMonth() + 1} / ${today.getDate()} / ${today.getFullYear()}`
 
+    // It would be nice if I could use xpath selectors here to test for a bit of structure...
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
       .assert.elementPresent('.date-header')
       .assert.containsText('h1', `Top Stories for ${todaysDate}`)
+      .assert.elementPresent('#story-list')
+      .waitForElementVisible('h3', 5000)
+      .assert.containsText('strong', 'U.S.')
+      .assert.containsText('h3', 'At the Crossroads of Church and Race, a Reporter Glimpses His Childhood - 3/9/2018, 2:00:01 AM')
       .end()
   },
 
