ba350ded486e3207838b64ee52d691dbff28e285
Scott Burton <spurton@gmail.com>
2018-03-09 17:41:49 -0800

    Lets test the data we control
    Modified the test to use the backupData so it won’t break in the future.

--- test/e2e/specs/test.js
+++ test/e2e/specs/test.js
@@ -6,30 +6,30 @@ module.exports = {
     // automatically uses dev Server port from /config.index.js
     // default: http://localhost:8080
     // see nightwatch.conf.js
-    const devServer = browser.globals.devServerURL
-    const today = new Date()
-    const todaysDate = `${today.getMonth() + 1} / ${today.getDate()} / ${today.getFullYear()}`
+    const devServer = browser.globals.devServerURL;
+    const today = new Date();
+    const todaysDate = `${today.getMonth() + 1} / ${today.getDate()} / ${today.getFullYear()}`;
 
     // It would be nice if I could use xpath selectors here to test for a bit of structure...
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.top-stories')
       .assert.elementPresent('.date-header')
       .assert.containsText('h1', `Top Stories for ${todaysDate}`)
-      .assert.elementPresent('#story-list')
-      .waitForElementVisible('h3', 5000)
-      .assert.containsText('strong', 'Briefing')
-      .assert.containsText('h3', 'North Korea, Florida, Martin Shkreli: Your Friday Evening Briefing - 3/9/2018, 3:04:32 PM')
-      .end()
+      .end();
   },
 
   'Top Stories' (browser) {
-    const devServer = browser.globals.devServerURL
+    const devServer = browser.globals.devServerURL;
 
     browser
       .url(devServer)
       .waitForElementVisible('#app', 5000)
-      .assert.elementPresent('.top-stories')
-      .end()
+      .assert.elementPresent('#story-list')
+      .waitForElementVisible('h3', 30000)
+      .assert.containsText('strong', 'Briefing')
+      .assert.containsText('h3', 'Florida, Rick Gates, Olympics: Your Weekend Briefing - 2/25/2018, 3:00:23 AM')
+      .end();
   }
 }
